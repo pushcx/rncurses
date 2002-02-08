6c76d9bfb8f48de08b6583af52536cfef5cc124f
t-peters <t-peters>
2002-02-08 10:44:06 +0000

    initial commit

---
+++ make_dist.rb
@@ -0,0 +1,21 @@
+def sys(i)
+  puts i
+  system i
+end
+
+Version = ARGV[0]
+dir  = File.dirname(__FILE__)
+base = File.basename(dir)
+base = "ncurses-ruby" if base == "."
+
+files = IO.readlines(dir + "/MANIFEST").collect{|filename|filename.chomp}
+
+sys "mkdir #{base}-#{Version}"
+files.each{|filename|
+  if filename.index "/"
+    sys "mkdir -p #{base}-#{Version}/#{File.dirname(filename)}"
+  end
+  sys "cp #{dir}/#{filename} #{base}-#{Version}/#{filename}"
+}
+sys "tar cIf #{base}-#{Version}.tar.bz2 #{base}-#{Version}"
+sys "rm -r #{base}-#{Version}/"
