22ee46df400c629f834004fd887295170a66a065
t-peters <t-peters>
2002-02-28 13:49:48 +0000

    Added copyright notice

--- extconf.rb
+++ extconf.rb
@@ -1,6 +1,23 @@
 #!/usr/bin/env ruby
 
-# $Id: extconf.rb,v 1.6 2002/02/26 11:04:38 t-peters Exp $
+# ncurses-ruby is a ruby module for accessing the FSF's ncurses library
+# (C) 2002 Tobias Peters <t-peters@users.berlios.de>
+#
+# This module is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This module is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this module; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+
+# $Id: extconf.rb,v 1.7 2002/02/28 13:49:48 t-peters Exp $
 
 require "mkmf"
 
