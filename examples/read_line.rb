7b305b0f0280d41250ffa4736dc5233068ac8a09
t-peters <t-peters>
2003-09-02 19:37:17 +0000

    - Calling singleton methods instead of private, included methods

--- examples/read_line.rb
+++ examples/read_line.rb
@@ -9,8 +9,6 @@
 
 require "ncurses"
 
-include Ncurses
-
 # read_line returns an array
 # [string, last_cursor_position_in_string, keycode_of_terminating_enter_key].
 # Complete the "when" clauses before including in your app!
@@ -24,13 +22,13 @@ def read_line(y, x,
     window.move(y,x+cursor_pos)
     ch = window.getch
     case ch
-    when KEY_LEFT
+    when Ncurses::KEY_LEFT
       cursor_pos = [0, cursor_pos-1].max
-    when KEY_RIGHT
-      # similar
-    when KEY_ENTER, ?\n, ?\r
+    when Ncurses::KEY_RIGHT
+      # similar, implement yourself !
+    when Ncurses::KEY_ENTER, ?\n, ?\r
       return string, cursor_pos, ch # Which return key has been used?
-    when KEY_BACKSPACE
+    when Ncurses::KEY_BACKSPACE
       string = string[0...([0, cursor_pos-1].max)] + string[cursor_pos..-1]
       cursor_pos = [0, cursor_pos-1].max
       window.mvaddstr(y, x+string.length, " ")
@@ -50,18 +48,20 @@ end
 
 if (__FILE__ == $0) then begin
   # demo mode
-  initscr
-  cbreak
-  noecho
-  keypad(stdscr, true) # for recognizing KEY_ENTER, KEY_BACKSPACE  etc
+  Ncurses.initscr
+  Ncurses.cbreak
+  Ncurses.noecho
+
+  # recognize KEY_ENTER, KEY_BACKSPACE  etc
+  Ncurses.keypad(Ncurses.stdscr, true)
   
   y = 10
   x = 2
   prompt = "Hallo > "
-  mvaddstr(y,x, prompt)
+  Ncurses.mvaddstr(y,x, prompt)
   s = read_line(y, x + prompt.length)
   
 ensure
-  endwin
+  Ncurses.endwin
 end end
 p s
